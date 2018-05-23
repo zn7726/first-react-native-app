6ed84717e4a379595ae39f968ab3e6006297e1b3
Ethan Zhang <ezhang@rawideas.com>
2018-05-23 16:48:13 +1000

    chapter45

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+import React from 'react';
 import { AppRegistry } from 'react-native';
 import App from './App';
-AppRegistry.registerComponent('firstreactnativeapp', () => App);
+import { Provider } from 'react-redux';
+import configureStore from './src/store/configureStore';
+
+const store = configureStore();
+
+const RNRedux = () => (
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
+
+AppRegistry.registerComponent('firstreactnativeapp', () => RNRedux);
---
+++ src/store/reducers/places.js
@@ -0,0 +1,49 @@
+import { ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE} from '../actions/actionTypes';
+const initialState = {
+    places: [],
+    selectedPlace: null
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_PLACE:
+            return {
+                ...state,
+                places: prevState.places.concat({
+                    key: Math.random(), 
+                    name: action,
+                    image: {
+                      uri: "https://www.sydney.com/sites/sydney/files/styles/full_height_image/public/2018-02/syd-1-1_0.jpg?itok=UawSK9dM"
+                    }
+                })
+            };
+        
+        case DELETE_PLACE:
+            return {
+                ...state,
+                places: state.places.filter(place => {
+                    return place.key !== state.selectedPlace.key;
+                }),
+                selectedPlace: null
+            }
+        
+        case SELECT_PLACE :
+            return {
+                ...state,
+                selectedPlace: state.places.find(place => {
+                    return place.key == action.placeKey;
+                })
+            };
+        
+        case DESELECT_PLACE:
+            return {
+                ... state,
+                selectedPlace: null
+            };
+
+        default:
+            return state; 
+    };
+};
+
+export default reducer;
