29e214c146e4daa1e89f2450be2872d6f3c0ee2c
Nan Zhang <zn7726@hotmail.com>
2018-05-25 17:50:12 +1000

    chapter 68

--- android/app/src/main/java/com/firstreactnativeapp/MainApplication.java
+++ android/app/src/main/java/com/firstreactnativeapp/MainApplication.java
@@ -10,38 +10,66 @@ import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
+
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.reactnativenavigation.NavigationApplication;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends NavigationApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
-
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
-        , new VectorIconsPackage()
-      );
-    }
-
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
+//  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+//    @Override
+//    public boolean getUseDeveloperSupport() {
+//      return BuildConfig.DEBUG;
+//    }
+//
+//    @Override
+//    protected List<ReactPackage> getPackages() {
+//      return Arrays.<ReactPackage>asList(
+//          new MainReactPackage()
+//        , new VectorIconsPackage()
+//      );
+//    }
+//
+//    @Override
+//    protected String getJSMainModuleName() {
+//      return "index";
+//    }
+//  };
+//
+//  @Override
+//  public ReactNativeHost getReactNativeHost() {
+//    return mReactNativeHost;
+//  }
+//
+//  @Override
+//  public void onCreate() {
+//    super.onCreate();
+//    SoLoader.init(this, /* native exopackage */ false);
+//  }
 
   @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
+  public boolean isDebug() {
+    // Make sure you are using BuildConfig from your own application
+    return BuildConfig.DEBUG;
+  }
+
+  protected List<ReactPackage> getPackages() {
+    // Add additional packages you require here
+    // No need to add RnnPackage and MainReactPackage
+    return Arrays.<ReactPackage>asList(
+            // eg. new VectorIconsPackage()
+        new VectorIconsPackage()
+    );
   }
 
   @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+  public List<ReactPackage> createAdditionalReactPackages() {
+    return getPackages();
   }
+
+  @Override
+  public String getJSMainModuleName() {
+    return "index";
+  }
+
 }
